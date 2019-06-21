dc4d867d7b99c5e0ffb9882d14ac22fc8963c728
Nery Tovar <ndtovare@gmail.com>
2019-06-21 18:06:21 -0500

    awertfyhui

--- src/app/operador/grids-llamadas/grids-llamadas.component.ts
+++ src/app/operador/grids-llamadas/grids-llamadas.component.ts
@@ -39,8 +39,8 @@ export class GridsLlamadasComponent implements OnInit {
   }
 
   public getLlamadasAtendidas(data: any) {
-    //let urlGetLlamadasAtendidas = 'http://3.14.155.2:9094/obtenerLlamadasAtendidas';
-    let urlGetLlamadasAtendidas = 'http://localhost:8999/obtenerLlamadasAtendidas';
+    let urlGetLlamadasAtendidas = 'http://3.14.155.2:9094/obtenerLlamadasAtendidas';
+    //let urlGetLlamadasAtendidas = 'http://localhost:8999/obtenerLlamadasAtendidas';
 
     this.gridsLlamadasService.getLlamadasAtendidas(urlGetLlamadasAtendidas, data).subscribe(
       response => {
