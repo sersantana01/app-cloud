cf92050a8ec83127e71f2cf5017bc92344e3ebd7
Francisco Galindo <ing.franciscoj.dg@gmail.com>
2019-07-01 14:39:27 -0500

    Evento recurrente

---
+++ src/app/shared/evento-recurrente/evento-recurrente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-evento-recurrente',
+  templateUrl: './evento-recurrente.component.html',
+  styleUrls: ['./evento-recurrente.component.css']
+})
+export class EventoRecurrenteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
